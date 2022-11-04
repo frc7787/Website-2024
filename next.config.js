62e6d347a1f20ddc2113e155c7b699a7f5f267a5
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-11-04 11:39:07 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  locales: ["en"],
-  defaultLocale: "en",
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 }
 
 module.exports = nextConfig
